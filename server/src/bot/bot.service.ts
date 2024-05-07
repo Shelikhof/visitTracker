e96914e3c99ee8099904f92f27f77dd7ac6ddeb8
Shelikhof <110841807+Shelikhof@users.noreply.github.com>
2024-05-07 21:39:04 +0300

    add summary table

--- server/src/bot/bot.service.ts
+++ server/src/bot/bot.service.ts
@@ -8,18 +8,11 @@ import { Context, Telegraf } from 'telegraf';
 export class BotService {
   constructor(@InjectBot() private readonly bot: Telegraf<Context>) {}
 
-  @Hears('да')
-  async onYes(@Ctx() ctx) {
-    log(ctx.from);
-    await ctx.reply('ПИЗДА ГАГАГА');
-  }
-
-  @Hears('иди нахуй')
-  async asd(@Ctx() ctx) {
-    log(ctx.from);
-    await ctx.reply('СОСИ ОЧКО');
-  }
-
+  // @Hears('тест')
+  // async onYes(@Ctx() ctx) {
+  //   log(ctx.from);
+  //   await ctx.reply('тест');
+  // }
   writeMessageToUser(id: number, message: string) {
     this.bot.telegram.sendMessage(id, message);
   }
